bba5bd336428f1c24c74f4eada276df6521b76d5
Mahmoud AlEbrahim <ma453935@gmail.com>
2024-12-31 07:29:15 +0300

    finish unit tests

---
+++ test/news_change_notifier_test.dart
@@ -0,0 +1,76 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing_tutorial/article.dart';
+import 'package:flutter_testing_tutorial/news_change_notifier.dart';
+import 'package:flutter_testing_tutorial/news_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+// Bad because we re implement the full class and its behavior
+// and if we want a different behavior for some tests we must
+// make another class and implement it.
+class BadMockNewsService  implements NewsService {
+  @override
+  Future<List<Article>> getArticles() async{
+    return [
+      Article(title: 'test 1', content: 'test 1 content'),
+      Article(title: 'test 2', content: 'test 2 content'),
+      Article(title: 'test 3', content: 'test 3 content'),
+    ];
+  }
+}
+
+class MockNewsService extends Mock implements NewsService {}
+
+void main() {
+  late NewsChangeNotifier sut;
+  late MockNewsService mockNewsService;
+
+  setUp(() {
+    mockNewsService = MockNewsService();
+    sut = NewsChangeNotifier(mockNewsService);
+  });
+
+  test(
+    "initial values are correct",
+        () {
+      expect(sut.articles, []);
+      expect(sut.isLoading, false);
+    },
+  );
+
+  group('getArticles', () {
+    final articlesFromService = [
+      Article(title: 'Test 1', content: 'Test 1 content'),
+      Article(title: 'Test 2', content: 'Test 2 content'),
+      Article(title: 'Test 3', content: 'Test 3 content'),
+    ];
+
+    void arrangeNewsServiceReturns3Articles() {
+      when(() => mockNewsService.getArticles()).thenAnswer(
+            (_) async => articlesFromService,
+      );
+    }
+
+    test(
+      "gets articles using the NewsService",
+          () async {
+        arrangeNewsServiceReturns3Articles();
+        await sut.getArticles();
+        verify(() => mockNewsService.getArticles()).called(1);
+      },
+    );
+
+    test(
+      """indicates loading of data,
+      sets articles to the ones from the service,
+      indicates that data is not being loaded anymore""",
+          () async {
+        arrangeNewsServiceReturns3Articles();
+        final future = sut.getArticles();
+        expect(sut.isLoading, true);
+        await future;
+        expect(sut.articles, articlesFromService);
+        expect(sut.isLoading, false);
+      },
+    );
+  });
+}
